1b10d54cc0a230b1495b737b6ed07934ef8c5065
nuriyantodev <nuriyantomail@gmail.com>
2023-02-09 14:10:22 +0700

    if function

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ This is my first project with python
 """
 print("Hello World")
 print("My name is nuriyanto")
+
+# Percabangan if
+tall = 170
+if tall >170:
+    print("high")
+else:
+    print("low")
